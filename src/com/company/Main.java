150e3e867669a607c4f691b53112b26e53074979
java-yzy-noob <754720227@qq.com>
2021-02-24 16:38:15 +0800

    test array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(" for the life");
-	// write your code here
+        MyArray myArray = new MyArray();
+        myArray.add(1);
+        myArray.add(2);
+        myArray.add(22);
+        myArray.add(2);
+        myArray.modify(2, 4);
+        myArray.add(100);
+        myArray.add(82);
+        myArray.delete(4);
+        myArray.display();
     }
 }
