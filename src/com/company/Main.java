54a7e229c0de7f855ef497ae0facba192bbdef9c
java-yzy-noob <754720227@qq.com>
2021-02-26 18:21:45 +0800

    test BubbleSort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,21 @@
 package com.company;
 
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        MyArray myArray = new MyArray();
-        myArray.add(1);
-        myArray.add(2);
-        myArray.add(22);
-        myArray.add(2);
-        myArray.modify(2, 4);
-        myArray.add(100);
-        myArray.add(82);
-        myArray.delete(4);
-        myArray.display();
+//        String s = "sdsasdasdadsadsadasdasdasdasdasdsadfasf";
+//        System.out.println(s.hashCode());
+//        int money = 1_000_000_000;   // int是32位的，一共有4_294_967_296种状态，补码世界中表示的范围是 -2_147_483_648 到 2_147_483_647
+//        int years = 20;
+//        int total = money * years;  // want = 20_000_000_000
+//        System.out.println(total);  // output = -1474836480
+
+        int a = -8;
+        System.out.println(a >>> 1);
+        System.out.println(Integer.MAX_VALUE);
     }
 }
